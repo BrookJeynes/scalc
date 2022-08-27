4943cc0330b2df35f721c3adeed874f06ec1468b
Brook <jeynesbrook@gmail.com>
2022-08-27 16:59:32 +1000

    removed build files

--- build/lib/scalc/callbacks.py
+++
@@ -1,7 +0,0 @@
-import typer
-from scalc import __settings__, __app_name__, __version__
-
-def _version_callback(value: bool) -> None:
-  if value:
-    typer.echo(f'{__app_name__} {__version__}')
-    raise typer.Exit()
--- build/lib/scalc/validate_ip.py
+++
@@ -1,8 +0,0 @@
-import ipaddress
-
-def validate_ip(address):
-    try: 
-        ipaddress.ip_address(address)
-        return True
-    except:
-        return False
